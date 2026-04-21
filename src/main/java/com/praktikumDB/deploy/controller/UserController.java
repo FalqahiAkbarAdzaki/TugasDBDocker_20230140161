96d9f99610ff5afeb93504606e54140da5915e4c
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2026-04-21 14:52:23 +0700

    UserController part 1

---
+++ src/main/java/com/praktikumDB/deploy/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.praktikumDB.deploy.controller;
+
+import com.praktikumDB.deploy.model.User;
+import com.praktikumDB.deploy.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping
+    public  String createUser(@RequestBody User request){
+        userService.addUser(request);
+        return "user created succesfully";
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id){
+        return userService.getUserByID(id);
+    }
+}
